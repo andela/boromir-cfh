80736b985f7edd9aec341939b278e59c691d3065
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-06-07 17:38:12 +0100

    chore(test): setup unit testing for frontend [Finishes #146622683]

---
+++ test/mocktest.js
@@ -0,0 +1,13 @@
+describe("true", function() {
+	it("Should be true", function () {
+		expect(true).toBeTruthy();
+	});
+});
+
+
+describe('sum', function() {
+	// body...
+	it('summing two numbers', function() {
+		expect(2 + 2).toEqual(4);
+	});
+});
